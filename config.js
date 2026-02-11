7f15a2c3640aba6b32e7192ca5ee4b98af5f46a0
M.Neto <152178564+Netox39@users.noreply.github.com>
2026-02-11 11:52:43 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.SUPABASE_URL = "https://zdjjswjlfbifhbwamgnt.supabase.co";
+
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpkampzd2psZmJpZmhid2FtZ250Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4MTg3ODksImV4cCI6MjA4NjM5NDc4OX0.LKWfzOpDCHgpJMERQnhINRoR8DvwOzBE1R_jcXf3s4I";
+
+window.ALLOW_SIGNUP = true;
