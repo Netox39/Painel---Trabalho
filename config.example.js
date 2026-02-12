7d2ed85985b4d847f95b6ea141dc9c892074132b
M.Neto <152178564+Netox39@users.noreply.github.com>
2026-02-12 08:22:53 -0300

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,5 +1,5 @@
 // Copie este arquivo para config.js e preencha com as suas chaves do Supabase.
 // IMPORTANTE: não commite o config.js (ele fica no .gitignore)
 
-window.SUPABASE_URL = "https://SEU-PROJETO.supabase.co";
-window.SUPABASE_ANON_KEY = "SUA_ANON_PUBLIC_KEY_AQUI";
+window.SUPABASE_URL = "https://zdjjswjlfbifhbwamgnt.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpkampzd2psZmJpZmhid2FtZ250Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4MTg3ODksImV4cCI6MjA4NjM5NDc4OX0.LKWfzOpDCHgpJMERQnhINRoR8DvwOzBE1R_jcXf3s4I";
